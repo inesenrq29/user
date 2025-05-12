4a0803383f2f7c7dad5cd63b3f52d8eb0df1d8be
Inès <ines.enrique@icloud.com>
2025-05-12 11:21:46 +0200

    ajout d'une croix pour fermer le chat + ajout du logo pour ouvrir le chat

--- Includes/head.php
+++ Includes/head.php
@@ -6,6 +6,7 @@
 
     <title>Chat Sneak-Me</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="stylesheet" href="<?= URL ?>public/assets/css/chat.css"/>
     <script src="<?= URL ?>public/assets/js/chat.js"></script>
 
